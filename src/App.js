40748739d5397b3f27bf9c88bd82770530139a70
Christian XU <christian.xu99@gmail.com>
2022-02-01 00:48:02 +0100

    chore(app): initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+function App() {   
+
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (value) => {
+    const boxValue = document.getElementById('box').value;
+    setTasks([...tasks, boxValue])
+  }
+
+  const clearAllTasks = () => {
+    const clearedTask = tasks.filter((task) => task === "")
+    return setTasks(clearedTask);
+  }
+
+  const deleteTask = (e) => {
+   //const newTasks = [...tasks];
+   //newTasks.splice(e.currentTarget.value, 1);
+   //setTasks(newTasks);
+
+    const reducedArray = tasks.filter((task, index) => index !== parseInt(e.currentTarget.value));
+    setTasks(reducedArray);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODO LIST</h1>
+      <input id="box"/>
+     <div>
+      <button  style= {{ backgroundColor: "green" }} onClick={() => addTask()}> ADD TASK </button>
+      <button  style= {{ backgroundColor: "red" }} onClick={() => clearAllTasks()}> CLEAR ALL TASK </button>
+     </div>
+      {tasks.map((task, index) => {
+        return (
+          <div key={task}>
+
+            <ul>
+              <li>{task}</li>
+            </ul>
+
+            <button value={index} style= {{ backgroundColor: "red" }} onClick={(e) => deleteTask(e)}>
+                DELETE
+            </button>
+
+          </div>
+        )
+      })}
     </div>
   );
 }
